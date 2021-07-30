b8006061ba1962a20204e3864cdd61ca947ccee9
Christopher Korbel <ckorbel1@gmail>
2021-07-29 17:22:43 -0700

    heat maps

--- heatmap/script.js
+++ heatmap/script.js
@@ -1,6 +1,6 @@
-async function draw(element) {
+async function draw(element, scale) {
   const data = await d3.json("data.json");
-
+  data.sort((a, b) => a - b);
   const dimensions = {
     width: 600,
     height: 150,
@@ -14,6 +14,32 @@ async function draw(element) {
     .attr("width", dimensions.width)
     .attr("height", dimensions.height);
 
+  // Scales
+  let colorScale;
+  if (scale === "linear") {
+    colorScale = d3
+      .scaleLinear()
+      .domain(d3.extent(data))
+      .range(["white", "red"]);
+  } else if (scale === "quantize") {
+    colorScale = d3
+      .scaleQuantize()
+      .domain(d3.extent(data))
+      .range(["white", "pink", "red"]);
+  } else if (scale === "quantile") {
+    colorScale = d3
+      .scaleQuantile()
+      .domain(data)
+      .range(["white", "pink", "red"]);
+
+    console.log("Quantize:", colorScale.quantiles());
+  } else if (scale === "threshold") {
+    // the threshold for upper class vs middle class
+    colorScale = d3
+      .scaleThreshold()
+      .domain([45200, 135600])
+      .range(["white", "pink", "red"]);
+  }
   // draw rectangles
   svg
     .append("g")
@@ -26,7 +52,11 @@ async function draw(element) {
     .attr("width", box - 3)
     .attr("height", box - 3)
     .attr("x", (d, i) => box * (i % 20))
-    .attr("y", (d, i) => box * ((i / 20) | 0));
+    .attr("y", (d, i) => box * ((i / 20) | 0))
+    .attr("fill", (d) => colorScale(d));
 }
 
-draw("#heatmap1");
+draw("#heatmap1", "linear");
+draw("#heatmap2", "quantize");
+draw("#heatmap3", "quantile");
+draw("#heatmap4", "threshold");
